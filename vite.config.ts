354a7ff88502f73fe28f51c0385e8735ccefa15e
zohaibbukhari126 <zohaibbukhari126@gmail.com>
2026-05-29 21:56:16 +0500

    fix-Vercel-preset

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,12 @@ export default defineConfig({
     // nitro/vite builds from this
     server: { entry: "server" },
   },
+  nitro: {
+    preset: "vercel",
+    output: {
+      dir: ".vercel/output",
+      serverDir: ".vercel/output/functions/__server.func",
+      publicDir: ".vercel/output/static",
+    },
+  },
 });
